766b43a0efe5218026b1a399218f172e71332ee2
ncevallos <nmcevall@gmail.com>
2020-11-25 16:43:56 -0500

    navbar fixes added favicon

---
+++ client/src/components/layout/Privacy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Privacy = () => {
+    return (
+        <section >
+            <div >
+                <div>
+                    <h1>BEA Better Eating</h1>
+                    <p>This is the Privacy Policy Page</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Privacy;
---
+++ client/src/components/layout/Termsconditions.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Termsconditions = () => {
+    return (
+        <section >
+            <div >
+                <div>
+                    <h1>BEA Better Eating</h1>
+                    <p>This is the Terms &amp; Conditions Page</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Termsconditions;
