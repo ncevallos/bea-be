22e87fc0e1ff24fe05687fd4952a1a28287394cc
ncevallos <nmcevall@gmail.com>
2021-01-21 12:05:24 -0500

    settings menu now works

--- client/src/components/layout/MeditateVidSection.js
+++ client/src/components/layout/MeditateVidSection.js
@@ -43,13 +43,8 @@ export const MeditateVidSection = () => {
                     <dd className="text-gray-500 text-sm">The more you chat with Bea, the better she’ll be able to identify what’s hindering your progress.</dd>
                     <dt className="sr-only">Role</dt>
                     </dl>
-<<<<<<< HEAD
-                    <button type="button" className="relative inline-flex items-center px-6 py-2 shadow-none text-sm font-medium rounded-full text-beaDarkPurple hover:bg-beaDarkPurple hover:text-white transition-all border border-beaDarkPurple group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>       
-=======
                     <button type="button" className="relative inline-flex items-center px-6 py-2 shadow-sm text-sm font-medium rounded-full text-beaDarkPurple hover:bg-beaDarkPurple hover:text-white transition-all border border-1 border-beaDarkPurple group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>       
->>>>>>> popups
                     Chat with Bea Now
                     </button>
                 </div>
