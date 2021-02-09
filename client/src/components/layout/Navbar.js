6504ceda71a9c20e3061bc7fb955e91dc30a9e24
ncevallos <nmcevall@gmail.com>
2021-02-09 11:27:42 -0500

    adding dropdown menu for mobile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,8 +19,7 @@ export const NavBar = () => {
             </div>
 
             <div className="flex xl:hidden">
-
-            <button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white" aria-expanded="false">
+            <Popup trigger={<button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
 
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -29,7 +28,21 @@ export const NavBar = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
 
-            </button>
+            </button>} position="bottom right">
+                {/* below here goes code for mobile menu  */}
+            <div className="px-2 pt-2 pb-3">
+                        <Link to='/' className="block px-3 py-2 rounded-md text-base font-medium text-white bg-indigo-800">Dashboard</Link>
+                        <Link to='/' className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600">Support</Link>
+                    </div>
+                    <div className="pt-4 pb-3 border-t border-indigo-800">
+                        <div className="px-2">
+                        <Link to='/' className="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600">Your Profile</Link>
+                        <Link to='/' className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600">Settings</Link>
+                        <Link to='/' className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600">Sign out</Link>
+                        </div>
+                    </div>
+                    </Popup>
+
             </div>
 
             <div className="hidden xl:block">
