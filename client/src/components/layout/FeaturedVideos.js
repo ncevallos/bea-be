33932910755f1055165f082f052ee7ab5ee39fc6
ncevallos <nmcevall@gmail.com>
2021-03-14 23:23:06 -0400

    Optimized meditate video data with mapping

--- client/src/components/layout/FeaturedVideos.js
+++ client/src/components/layout/FeaturedVideos.js
@@ -1,49 +1,30 @@
 
 import React from 'react'
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import peskyPatterns from '../../img/pesky-patterns.svg';
 import Videoselection from './Videoselection';
-import clouds from '../../img/meditateimgs/clouds.png';
-import cracklingfire from '../../img/meditateimgs/cracklingfire.png';
-import lazyafternoon from '../../img/meditateimgs/lazyafternoon.png';
-import monks from '../../img/meditateimgs/monks.png';
-import moonlight from '../../img/meditateimgs/moonlight.png';
-import mountainstream from '../../img/meditateimgs/mountainstream.png';
-import orangegrove from '../../img/meditateimgs/orangegrove.png';
-import sailboat from '../../img/meditateimgs/sailboat.png';
-import twowomen from '../../img/meditateimgs/two-women.png';
-import woods from '../../img/meditateimgs/woods.png';
+import { Videodata } from "./Videodata";
 
 
 class FeaturedVideos extends Component {
-    state = {
-        videolist: [
-            {id: "1", title: 'Clouds', length: '5 minutes', img: clouds, imgalt: "Clouds"},
-            {id: "2", title: 'Monks', length: '7 minutes', img: monks, imgalt: "Monks", vidurl: "https://vimeo.com/447565057/8cb1bf83c1"},
-            {id: "3", title: 'Woods', length: '3 minutes', img: woods, imgalt: "Woods"},
-            {id: "4", title: 'Moonlight', length: '3 minutes', img: moonlight, imgalt: "Moonlight"},
-            {id: "5", title: 'Lazy Afternoon', length: '3 minutes', img: lazyafternoon, imgalt: "Lazy Afternoon"},
-            {id: "6", title: 'Crackling Fire', length: '3 minutes', img: cracklingfire, imgalt: "Crackling Fire"},
-            {id: "7", title: 'Sailboat', length: '5 minutes', img: sailboat, imgalt: "Sailboat"},
-            {id: "8", title: 'Two Women', length: '7 minutes', img: twowomen, imgalt: "Two Women", vidurl: "https://vimeo.com/515546507/2fd2b94c40"},
-            {id: "9", title: 'Orange Grove', length: '7 minutes', img: orangegrove, imgalt: "Orange Grove", vidurl: "https://player.vimeo.com/video/515546782"}
-        ]
-    }
     render() {
     return (
 
 
             <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mr-4 gap-4 w-full">
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[0].title} length={this.state.videolist[0].length} img={this.state.videolist[0].img} imgalt={this.state.videolist[0].imgalt}/>
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[1].title} length={this.state.videolist[1].length} img={this.state.videolist[1].img} imgalt={this.state.videolist[1].imgalt}   vidurl={this.state.videolist[1].vidurl} />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[2].title} length={this.state.videolist[2].length} img={this.state.videolist[2].img} imgalt={this.state.videolist[2].imgalt}  />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[3].title} length={this.state.videolist[3].length} img={this.state.videolist[3].img} imgalt={this.state.videolist[3].imgalt}  />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[4].title} length={this.state.videolist[4].length} img={this.state.videolist[4].img} imgalt={this.state.videolist[4].imgalt} />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[5].title} length={this.state.videolist[5].length} img={this.state.videolist[5].img} imgalt={this.state.videolist[5].imgalt}  />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[6].title} length={this.state.videolist[6].length} img={this.state.videolist[6].img} imgalt={this.state.videolist[6].imgalt}  />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[7].title} length={this.state.videolist[7].length} img={this.state.videolist[7].img} imgalt={this.state.videolist[7].imgalt} vidurl={this.state.videolist[7].vidurl}   />
-                <Videoselection id={this.state.videolist[0].id} title={this.state.videolist[8].title} length={this.state.videolist[8].length} img={this.state.videolist[8].img} imgalt={this.state.videolist[8].imgalt} vidurl={this.state.videolist[8].vidurl} />
+                        {Videodata.map((data, key) => {
+                            return (
+                                <div key={key}>
+                                <Videoselection
+                                    id={data.id}
+                                    title={data.title}
+                                    length={data.length}
+                                    img={data.img}
+                                    imgalt={data.imgalt}
+                                    vidurl={data.vidurl}
+                                />
+                                </div>
+                            );
+                            })}
             </ul>
 
 
---
+++ client/src/components/layout/Videodata.js
@@ -0,0 +1,82 @@
+export const Videodata = [
+        {
+            id: "1", 
+            title: 'Clouds', 
+            length: '5 minutes', 
+            img: "/static/media/clouds.fe3d8740.png", 
+            imgalt: "Clouds",
+            vertimg: ""
+        },
+        {
+            id: "2", 
+            title: 'Monks', 
+            length: '7 minutes', 
+            img: "/static/media/monks.f2ba32e1.png",
+            imgalt: "Monks", 
+            vidurl: "https://vimeo.com/447565057/8cb1bf83c1",
+            vertimg: ""
+        },
+        {
+            id: "3", 
+            title: 'Woods', 
+            length: '3 minutes', 
+            img: "/static/media/woods.c291fec7.png", 
+            imgalt: "Woods",
+            vertimg: ""
+        },
+        {
+            id: "4", 
+            title: 'Moonlight', 
+            length: '3 minutes', 
+            img: "/static/media/moonlight.cb8e7de2.png", 
+            imgalt: "Moonlight",
+            vertimg: ""
+        },
+        {
+            id: "5", 
+            title: 'Lazy Afternoon', 
+            length: '3 minutes', 
+            img: "/static/media/lazyafternoon.6738f57d.png", 
+            imgalt: "Lazy Afternoon",
+            vertimg: ""
+       },
+        {
+            id: "6", 
+            title: 'Crackling Fire', 
+            length: '3 minutes', 
+            img: "/static/media/cracklingfire.4704d17b.png", 
+            imgalt: "Crackling Fire",
+            vertimg: ""
+        },
+        {
+            id: "7", 
+            title: 'Sailboat', 
+            length: '5 minutes', 
+            img: "/static/media/sailboat.9b027d9a.png", 
+            imgalt: "Sailboat",
+            vertimg: ""
+        },
+        {
+            id: "8", 
+            title: 'Two Women', 
+            length: '7 minutes', 
+            img: "/static/media/two-women.bd80e1f5.png", 
+            imgalt: "Two Women", 
+            vidurl: "https://vimeo.com/515546507/2fd2b94c40",
+            vertimg: ""
+        },
+        {
+            id: "9", 
+            title: 'Orange Grove', 
+            length: '7 minutes', 
+            img: "/static/media/orangegrove.8824e9b3.png", 
+            imgalt: "Orange Grove", 
+            vidurl: "https://player.vimeo.com/video/515546782",
+            vertimg: ""
+    }
+
+    
+  ];
+
+
+export default Videodata;
