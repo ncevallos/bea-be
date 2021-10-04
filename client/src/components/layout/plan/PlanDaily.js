9929b6686e2c472c26a134b64e1a91a3e83c48d1
andershoedholt <anders@hoedholt.me>
2021-10-04 17:45:38 +0200

    Updates to Plan Daily

--- client/src/components/layout/plan/PlanDaily.js
+++ client/src/components/layout/plan/PlanDaily.js
@@ -47,7 +47,7 @@ import { getResultsByIdToday } from '../../../actions/postPlanResults';
             {/* {todayPlanResults.map((todayPlanResult) => ( */}
                 <div className="flex flex-col flex-grow">
 
-                    <div className="dateHeader text-center items-center mb-3">
+                    <div className="dateHeader text-center items-center mb-gutter">
                         <label className="flex content-center mb-0">   
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
@@ -56,33 +56,35 @@ import { getResultsByIdToday } from '../../../actions/postPlanResults';
                         </label>
                     </div>
 
-                    <div className="flex bg-white overflow-hidden border border-gray-400 rounded-xl p-4 mb-3 items-start">
+                    <div className="flex flex-col bg-white overflow-hidden border border-gray-400 rounded-xl p-4 mb-gutter items-start">
+                        <div className="flex justify-center w-full">
+                            <span className="text-beaBlueText font-bold uppercase text-sm">Mood Eating</span>
+                        </div>
                         {/* Below needs to implemented to go full width according to designs
                         <div className="flex-1 flex flex-col p-8 text-center">Mood Eating</div> */}
-                        <img src={smileyIconRound} alt="Round Smiley Icon" className="w-36" />
-                        <div className="px-4 py-3 sm:px-6">
-                            <p className="mt-1 max-w-2xl text-sm text-gray-500 mb-0">
-                                How you felt about what you ate in the past day
-                            </p>
-                            <h3 className="text-4xl font-medium text-gray-900 font-extralight mb-3">
-                                {todayPlanResults.howdoyoufeel}
-                            </h3>
-                            <div className="rounded-md bg-beaLightBlue  p-4">
-                                <div className="flex">
-                                    <div className="flex-shrink-0">
+                        <div className="flex flex-row items-start w-full">
+                            <img src={smileyIconRound} alt="Round Smiley Icon" className="w-24" />
+                            <div className="flex flex-col items-baseline px-4 py-3 sm:px-6">
+                                <p className="mt-1 max-w-2xl text-sm text-gray-500 mb-0">
+                                    How you felt about what you ate in the past day
+                                </p>
+                                <h3 className="text-4xl font-light text-gray-900 font-extralight mb-3">
+                                    {todayPlanResults.howdoyoufeel}
+                                </h3>
+                                <div className="flex flex-row items-center mb-1.5">
+                                    <div className="flex flex-none rounded-full bg-beaLightBlue p-2">
                                         <img src={heartIcon} alt="Heart Icon" className="w-5" />
                                     </div>
                                     <div className="ml-3 flex-1 md:flex md:justify-between">
-                                        <p className="text-sm text-blue-700 mb-0">
+                                        <p className="text-sm text-gray-500 mb-0">
                                             <strong>{todayPlanResults.stayedontrack}</strong> helped you stay on track for the following reason:
                                         </p>
                                     </div>
                                 </div>
+                                <p className="mt-1 text-sm text-gray-700 leading-6">
+                                    {todayPlanResults.planjournal}
+                                </p>
                             </div>
-                            <p className="mt-1 text-sm text-gray-700 max-w-prose leading-6">
-                            {todayPlanResults.planjournal}
-                            
-                            </p>
                         </div>
                     </div>
 
@@ -90,41 +92,56 @@ import { getResultsByIdToday } from '../../../actions/postPlanResults';
 
 
                     <div>
-                        <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
+                        <ul className="grid grid-cols-1 gap-gutter sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 mb-gutter">
                             <li className="col-span-1 flex flex-col text-center bg-white rounded-xl divide-y divide-gray-200 border border-gray-400">
                                 <div className="flex-1 flex flex-col p-8 items-center">
                                     <span className="text-beaBlueText text-sm">Goal</span>
-                                    <span className="mt-6 p-3 bg-beaLightBlue rounded-3xl flex items-center justify-center">
+                                    <span className="mt-6 p-4 bg-beaLightBlue rounded-3xl flex items-center justify-center">
                                         <img src={bowlIcon} alt="Bowl Icon" className="w-11" />
                                     </span>
-                                    <span className="mt-6 text-gray-700 text-sm font-thin">The most important goal for you to have today</span>
-                                    <h2 className="mt-0 text-gray-700 text-3xl font-light">{todayPlanResults.plangoal} and<br/>
-                                                        {todayPlanResults.plangoal2}</h2>
-                                    <span className="mt-6 p-2 border-2 border-beaLightBlue rounded-2xl flex items-center justify-center">
-                                        <img src={newspaperIcon} alt="Newspaper Icon" className="w-7" />
-                                    </span>
-                                    <span className="mt-1 text-gray-700 text-sm font-bold uppercase tracking-wider">BEA’S RECOMMENDED READ</span>
-                                    <span className="mt-1 mb-3 text-gray-700 text-sm font-light">Healthy Eating: Recognizing Your Hunger Signals</span>
-                                    <button type="button" className="btn-darkPurple">       
-                                        Read Article
-                                    </button>
+                                    <span className="mt-6 text-gray-700 text-sm font-light">The most important goal for you to have today</span>
+                                    <h2 className="mt-0 text-gray-700 text-3xl font-light mb-3">{todayPlanResults.plangoal} and {todayPlanResults.plangoal2}</h2>
+                                    <div className="flex flex-col items-center bg-beaStrongOrange w-full rounded-3xl p-1.5">
+                                        <div className="bg-opacity-60 bg-gray-100 w-full rounded-3xl p-3 uppercase text-beaStrongOrange font-bold text-lg mb-3">Bea's Tips</div>
+                                        <span className="text-white text-md mb-1.5"><strong>You may find this article helpful:</strong> Healthy Eating: Recognizing Your Hunger Signals.</span>
+                                        <svg className="w-6 mb-2.5" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2.33454 14.3192L22.6536 14.3192" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.3777 2.33984L23.2356 14.2705L13.3777 26.2982" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+
+                                    </div>
                                 </div>
                             </li>
                             <li className="col-span-1 flex flex-col text-center bg-white rounded-xl divide-y divide-gray-200 border border-gray-400">
                                 <div className="flex-1 flex flex-col p-8 items-center">
                                     <span className="text-beaBlueText text-sm">Action</span>
-                                    <span className="mt-6 p-3 bg-beaLightBlue rounded-3xl flex items-center justify-center">
+                                    <span className="mt-6 p-4 bg-beaLightBlue rounded-3xl flex items-center justify-center">
                                         <img src={bowlIcon}  alt="Bowl Icon" className="w-11" />
                                     </span>
-                                    <span className="mt-6 text-gray-700 text-sm font-thin">When you feel that urge to eat</span>
+                                    <span className="mt-6 text-gray-700 text-sm font-light">When you feel that urge to eat</span>
                                     <h2 className="mt-0 text-gray-700 text-3xl font-light">{todayPlanResults.plangoal5}</h2>
                                 </div>
                             </li>
                         </ul>
                     </div>
 
-                    <Link to={yesterday} className="dateHeader mb-2">{moment(todayPlanResults.date).subtract(1, 'days').format('MMMM Do')}</Link>
-                    <Link to='/PlanMonthly' className="dateHeader mb-2">View {moment(todayPlanResults.date).format('MMMM')}</Link>
+                    <div className="flex flex-row justify-between">
+                        <Link to={yesterday} className="flex flex-row items-center navigateButton px-3">
+                            <svg className="w-4 mr-1.5 ml-0" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.74 8.21045L2.78 8.21045" stroke="#7B48CB" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M8.23996 13.9604L2.4375 8.23375L8.23996 2.46045" stroke="#7B48CB" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            {moment(todayPlanResults.date).subtract(1, 'days').format('MMMM Do')}
+                        </Link>
+                        <Link to='/PlanMonthly' className="flex flex-row items-center navigateButton px-3">
+                            View {moment(todayPlanResults.date).format('MMMM')}
+                            <svg className="w-4 ml-1.5" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.45043 7.78955L14.4104 7.78955" stroke="#7B48CB" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M8.95047 2.03955L14.7529 7.76625L8.95047 13.5396" stroke="#7B48CB" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </Link>
+                    </div>
+
                     {nextDayButton}
                     {/* <Link to='/PlanMonthly' className="dateHeader">{moment(todayPlanResults.date).add(1, 'days').format('MMMM Do')}</Link> */}
 
@@ -132,14 +149,14 @@ import { getResultsByIdToday } from '../../../actions/postPlanResults';
                 </div>
 
 {/* ))} */}
-                <div className="w-72 min-w-72 pl-3">
-                    <div className="flex justify-center items-center w-full h-72 rounded-xl bg-gray-300 w-72">
-                        <button type="button" className="relative inline-flex items-center px-6 py-2 shadow-none text-sm font-medium rounded-full text-beaDarkPurple hover:bg-beaDarkPurple hover:text-white transition-all border-t border-b border-l border-r border-beaDarkPurple group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
-                        Call to Action
-                        </button>
-                    </div>
-                </div>  
+                  <div className="flex flex-shrink-0 ml-gutter w-promo">
+                      <div className="flex justify-center items-center w-full h-72 rounded-md bg-gray-300 w-full p-2">
+                          <button type="button" className="relative inline-flex items-center px-6 py-2 shadow-none text-sm font-medium rounded-full text-beaDarkPurple hover:bg-beaDarkPurple hover:text-white transition-all border-t border-b border-l border-r border-beaDarkPurple group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                          <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
+                          Call to Action
+                          </button>
+                      </div>
+                  </div>
             </div>
         </section>
     )
