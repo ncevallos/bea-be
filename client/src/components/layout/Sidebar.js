b5072c742bd4954ea9aff714a3528367ec5f91d6
andershoedholt <anders@hoedholt.me>
2022-01-08 16:30:40 +0100

    Styling fix (5)

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -99,7 +99,7 @@ export const Sidebar = () => {
                   <Link to='/Chat' className={"group rounded-2xl lg:w-full p-3 lg:px-5 px-2.5 transition-bg bg-gray-200 hover:no-underline " + ((activePage === "Chat") ? "bg-beaPurpleCorrected text-white" : 'text-beaTextGrey')}>
                     <div className="flex flex-col lg:flex-row items-center lg:space-x-2 lg:transition-spacing lg:transform lg:group-hover:pl-2">
                       <svg className={activePage === "Chat" ? "w-6 h-6 text-white"  : "w-6 h-6 text-beaPurpleCorrected"} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
-                      <span className={"text-sm font-medium text-beaPurpleCorrected " + ((activePage === "Chat") ? "text-white" : 'text-beaPurpleCorrected')}>Chat with Bea</span>
+                      <span className={"text-sm font-medium text-beaPurpleCorrected " + ((activePage === "Chat") ? "text-white" : 'text-beaPurpleCorrected')}>Chat with Bea (test)</span>
                     </div>
                   </Link>                  
                 </div>
